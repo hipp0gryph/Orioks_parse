c2019710b3c7eab526af34a7024568e03cd2da89
Daniil Pozdnyakov <dpozdnyakov@psystems.biz>
2022-09-18 16:28:39 +0300

    add(main, config): add code for work with exceptions, new config logic and upload to mongodb

--- config.py
+++ config.py
@@ -1,2 +1,16 @@
-login = "rainbow123"
-pas = "my_secret_password"
+import configparser
+
+
+class Config(object):
+    __config = None
+    __instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__config = configparser.ConfigParser()
+            cls.__config.read("config.conf")
+            cls.__instance = super(Config, cls).__new__(cls)
+        return cls.__instance
+
+    def __getitem__(self, key):
+        return Config.__config[key]
--- main.py
+++ main.py
@@ -3,78 +3,65 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import config
 import time
-import sqlite3
-
-import spisok
+from pymongo import MongoClient
+from config import Config
 import json
+import asyncio
 
 
-class DatabaseLoad:
+class Load:
     def __init__(self):
-        self.db = sqlite3.connect('db.sqlite')
+        config = Config()
+        self.client = MongoClient(config["MONGO_DB"]["host"])
+        self.db = self.client.get_database(config["MONGO_DB"]["db"])
+        self.collection = self.db.get_collection(config["MONGO_DB"]["table"])
 
-    def load(self, json):
-        for obj in json:
-            columns = []
-            column = []
-            for data in obj:
-                print(data)
-                column = list(data.keys())
-            for col in column:
-                if col not in columns:
-                    columns.append(col)
-
-            value = []
-            values = []
-            for data in obj:
-                for i in columns:
-                    value.append(str(dict(data).get(i)))
-            values.append(list(value))
-            value.clear()
-
-            create_query = "create table if not exists myTable ({0})".format(" text,".join(columns))
-            insert_query = "insert into myTable ({0}) values(?{1})".format(", ".join(columns), ",?" * (len(columns) - 1))
-            c = self.db.cursor()
-            c.execute(create_query)
-            c.executemany(insert_query, values)
-            values.clear()
-            self.db.commit()
-            c.close()
+    def insert_many(self, data):
+        self.collection.insert_many(data)
 
 
 class OrioksParse:
     def __init__(self):
+        self.loader = Load()
         self.d = {"Empty": []}
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.implicitly_wait(5)
         self.driver.get('https://orioks.miet.ru/user/login')
         element = self.driver.find_elements(By.CSS_SELECTOR, 'input')
-        element[1].send_keys(config.login)
-        element[2].send_keys(config.pas)
+        config = Config()
+        element[1].send_keys(config['DEFAULT']['user'])
+        element[2].send_keys(config['DEFAULT']['pass'])
         time.sleep(2)
         element[2].submit()
         time.sleep(2)
 
-    def __get_data(self, num):
+    def __get_data(self, num, check=0):
+        self.driver.get('https://orioks.miet.ru/portfolio/view-project?id_project=' + str(num))
         try:
-            self.driver.get('https://orioks.miet.ru/portfolio/view-project?id_project=' + str(num))
-            time.sleep(2)
             return self.driver.find_element(By.CSS_SELECTOR, "#forang").get_attribute("innerText")
         except:
-            self.__get_data(num)
+            time.sleep(1)
+            check += 1
+            if check == 3:
+                return None
+            self.__get_data(num, check)
 
-    def get_list_works(self, min, max):
+    def parse(self, min, max):
         elements = list()
         for i in range(min, max + 1):
-            elements.append(json.loads(self.__get_data(i)))
-        return elements
+            data = self.__get_data(i)
+            if data is not None:
+                try:
+                    elements.append(json.loads(data))
+                    self.loader.insert_many(elements)
+                except:
+                    pass
 
 
 if __name__ == "__main__":
+    max = int(input("Enter max, please: "))
+    min = int(input("Enter min, please: "))
     parser = OrioksParse()
-    json = parser.get_list_works(0, 5)
-    print(json)
-    for i in json:
-        print(i)
-    loader = DatabaseLoad()
-    loader.load(json)
+    for i in range(min // 50, max // 50):
+        parser.parse(50 * i, (50 * (i + 1) - 1))
+        print(f"Iteration now: {i}.\nRows inserted: {50 * (i + 1)}")
